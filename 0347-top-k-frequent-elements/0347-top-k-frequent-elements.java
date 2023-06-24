2fb9671985302e959083435b14dfb6800c567f5f
Ahmed Haitham <adhaithaam@gmail.com>
2023-06-24 21:52:19 +0300

    Time: 14 ms (75.16%), Space: 47.8 MB (56.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,16 @@
 class Solution {
-   public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> Map = new HashMap<Integer, Integer>();
-        HashSet<Integer> Set = new HashSet<Integer>();
+public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
         for (int num : nums) {
-            if (!Map.containsKey(num)) {
-                Map.put(num, 1);
-            } else {
-                Map.put(num, Map.get(num) + 1);
-            }
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        int[] freq = Map.values().stream().mapToInt(i -> i).sorted().toArray();
+        List<Map.Entry<Integer, Integer>> freqList = new ArrayList<>(freqMap.entrySet());
+        freqList.sort((a, b) -> b.getValue() - a.getValue());
+        int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            for (Integer key : Map.keySet()
-            ) {
-                if (Map.get(key) == freq[freq.length - 1 - i]) {
-                    Set.add(key);
-                }
-            }
+            result[i] = freqList.get(i).getKey();
         }
 
-        return Set.stream().mapToInt(i -> i).toArray();
+        return result;
     }
 }
