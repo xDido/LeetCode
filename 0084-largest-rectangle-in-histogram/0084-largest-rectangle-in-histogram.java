c1df796d1d406efcad863aaafef6018016e79dfe
Ahmed Haitham <adhaithaam@gmail.com>
2023-07-09 09:20:13 +0300

    Time: 271 ms (34.61%), Space: 55.2 MB (81.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,28 +1,44 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-      int area = 0, n = heights.length;
-      int start;
-      Stack<Pair<Integer,Integer>> stack = new Stack<>();
-      for(int i=0;i<heights.length;i++){
-         int curHt =heights[i];
-         start = i;
-        while(!stack.isEmpty() && stack.peek().getValue() > curHt){
-          Pair<Integer,Integer> pair = stack.pop();
-          int index = pair.getKey();
-          int h = pair.getValue();
-          area = Math.max(area, h * (i - index));
-          start = index;
-        }
-        stack.push(new Pair(start,curHt));
-      }
-      
-       while(!stack.isEmpty()){
-          Pair<Integer,Integer> pair = stack.pop();
-          int index = pair.getKey();
-          int h = pair.getValue();
-          area = Math.max(area, h * (n - index));
-       }
-        return area;
+public int largestRectangleArea(int[] heights) {
+    int n = heights.length;
+    int[] left = new int[n]; // Stores the left boundaries
+    int[] right = new int[n]; // Stores the right boundaries
+
+    // Initialize the right boundaries
+    for (int i = 0; i < n; i++) {
+        right[i] = n;
     }
-  
+
+    Stack<Integer> stack = new Stack<>();
+
+    // Calculate the left boundaries
+    for (int i = 0; i < n; i++) {
+        while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+            stack.pop();
+        }
+        left[i] = stack.isEmpty() ? 0 : stack.peek() + 1;
+        stack.push(i);
+    }
+
+    stack.clear();
+
+    // Calculate the right boundaries
+    for (int i = n - 1; i >= 0; i--) {
+        while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+            stack.pop();
+        }
+        right[i] = stack.isEmpty() ? n : stack.peek();
+        stack.push(i);
+    }
+
+    int area = 0;
+
+    // Calculate the maximum area
+    for (int i = 0; i < n; i++) {
+        area = Math.max(area, heights[i] * (right[i] - left[i]));
+    }
+
+    return area;
+}
+
 }
