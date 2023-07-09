61d0f6cf10dc5e3e47896854c0b76af201ac5ccc
Ahmed Haitham <adhaithaam@gmail.com>
2023-07-09 09:33:32 +0300

    Time: 0 ms (100.00%), Space: 45 MB (40.35%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int mid;
+        int end = nums.length - 1;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+        }
+        return -1;
+    }
+}
