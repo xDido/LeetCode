ee8b79841642ded1dbd593cd7fb28add972f9849
Ahmed Haitham <adhaithaam@gmail.com>
2023-07-09 08:24:52 +0300

    Time: 24 ms (95.16%), Space: 56.3 MB (59.92%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,25 +1,21 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int[][] pairs = new int[position.length][2];
+        if (position.length == 1 || position.length == 0)
+            return position.length;
+        double[] time = new double[target];
         int fleets = 0;
-        double slowest = 0;
         for (int i = 0; i < position.length; i++) {
-            pairs[i][0] = position[i];
-            pairs[i][1] = speed[i];
+            time[position[i]] = (double)(target - position[i]) / speed[i];
         }
-        Arrays.sort(pairs, java.util.Comparator.comparingInt(o -> o[0]));
-        for (int i = pairs.length - 1; i > -1; i--) {
-            double currTime = (double) (target - pairs[i][0]) / pairs[i][1];
-            if (currTime > slowest) {
-                slowest = currTime;
+        double slowest = 0;
+        for (int i = time.length - 1; i >= 0; i--) {
+            if (time[i] > slowest) {
+                slowest = time[i];
                 fleets++;
-            } else {
-                continue;
             }
-
-
         }
         return fleets;
+
 
     }
 }
