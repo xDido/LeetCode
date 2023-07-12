514d11ed3b24de1d6da8ed603ea41726fa05ab4c
Ahmed Haitham <adhaithaam@gmail.com>
2023-07-12 14:05:21 +0300

    Time: 2 ms (100.00%), Space: 44.8 MB (54.35%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,52 +1,33 @@
 class Solution {
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-
-        if (m > n) {
+        if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
-
-        int total = m + n;
+        int l = 0;
+        int r = nums1.length;
+        int total = nums1.length + nums2.length;
         int half = (total + 1) / 2;
-
-        int left = 0;
-        int right = m;
-
-        var result = 0.0;
-
-        while (left <= right) {
-            int i = left + (right - left) / 2;
-            int j = half - i;
-
-            // get the four points around possible median
-            int left1 = (i > 0) ? nums1[i - 1] : Integer.MIN_VALUE;
-            int right1 = (i < m) ? nums1[i] : Integer.MAX_VALUE;
-            int left2 = (j > 0) ? nums2[j - 1] : Integer.MIN_VALUE;
-            int right2 = (j < n) ? nums2[j] : Integer.MAX_VALUE;
-
-            // partition is correct
-            if (left1 <= right2 && left2 <= right1) {
-                // even
+        double res = 0;
+        while (true) {
+            int mid = l + (r - l) / 2;
+            int midB = half - mid;
+            int element_leftA = (mid > 0) ? nums1[mid - 1] : Integer.MIN_VALUE;
+            int element_rightA = (mid < nums1.length) ? nums1[mid] : Integer.MAX_VALUE;
+            int element_leftB = (midB > 0) ? nums2[midB - 1] : Integer.MIN_VALUE;
+            int element_rightB = (midB < nums2.length) ? nums2[midB] : Integer.MAX_VALUE;
+            if (element_leftA <= element_rightB && element_leftB <= element_rightA) {
                 if (total % 2 == 0) {
-                    result =
-                            (Math.max(left1, left2) + Math.min(right1, right2)) /
-                                    2.0;
-                    // odd
+                    res = (double) (Math.max(element_leftA, element_leftB) + Math.min(element_rightA, element_rightB)) / 2;
                 } else {
-                    result = Math.max(left1, left2);
+                    res = (double) Math.max(element_leftA, element_leftB);
                 }
                 break;
-            }
-            // partition is wrong (update left/right pointers)
-            else if (left1 > right2) {
-                right = i - 1;
+            } else if (element_leftA > element_rightB) {
+                r = mid - 1;
             } else {
-                left = i + 1;
+                l = mid + 1;
             }
         }
-
-        return result;
+        return res;
     }
 }
